667a7979164c083748e35d1d90c7dba046192c22
ordamari <ordamari971@gmail.com>
2020-11-08 21:16:20 +0200

    Updates

---
+++ precache-manifest.8944f8f17b876e078f9bad754154d6ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fd3cac94fe7471fa749491680fe820b",
+    "url": "/task-manager/./index.html"
+  },
+  {
+    "revision": "f7ff2b26c3ffc4c2a95f",
+    "url": "/task-manager/./static/css/main.35ef5f19.chunk.css"
+  },
+  {
+    "revision": "9c874d7dbe4d1c8659fb",
+    "url": "/task-manager/./static/js/2.31c99b63.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/task-manager/./static/js/2.31c99b63.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f7ff2b26c3ffc4c2a95f",
+    "url": "/task-manager/./static/js/main.115c8fbf.chunk.js"
+  },
+  {
+    "revision": "0928e1e8513c2ef021e7",
+    "url": "/task-manager/./static/js/runtime-main.935b9f5b.js"
+  },
+  {
+    "revision": "b20ff458bc0dc48c039c68a3dc96173c",
+    "url": "/task-manager/./static/media/Caveat-Regular.b20ff458.ttf"
+  },
+  {
+    "revision": "9406d0ab606cf8cb91c41b65556bd836",
+    "url": "/task-manager/./static/media/Lobster-Regular.9406d0ab.ttf"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/task-manager/./static/media/Montserrat-Regular.ee653992.ttf"
+  }
+]);
